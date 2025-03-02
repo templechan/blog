5534fb9042cfd5de18f0801f9a7e4e640be3fd28
templechan <templechan@126.com>
2025-03-02 09:06:49 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,34 @@
 cd /usr/local/src
 rm -rf /usr/local/src/blog
-dnf install -y git
-git config --global user.email "templechan@126.com"
-git config --global user.name "templechan"
+
+if ! command -v git &> /dev/null; then
+    dnf install -y git
+    git config --global user.email "templechan@126.com"
+    git config --global user.name "templechan"
+fi
 # 使用 ssh 克隆会快一些
 git clone -b main git@github.com:templechan/blog.git
 # 使用加速
 # git clone -b main https://gitclone.com/github.com/templechan/blog.git
 
-cd /usr/local/src/blog
-# 手动压缩图片资源（会覆盖源文件，注意保留源文件，同一个文件多次压缩会严重失真）
-# 也可指定具体文件名压缩
-dnf install -y ImageMagick
-mogrify -resize 20% -quality 70 ./static/img/*.{png}
+if [-d /usr/local/src/blog]; then
+    cd /usr/local/src/blog
+    # 手动压缩图片资源（会覆盖源文件，注意保留源文件，同一个文件多次压缩会严重失真）
+    # 也可指定具体文件名压缩
+    dnf install -y ImageMagick
+    mogrify -resize 20% -quality 70 ./static/img/*.{png}
 
-# 删除容器
-docker stop blog && docker rm blog
-# 删除镜像
-docker rmi blog
-# 构建镜像
-docker build -t blog .
-# 创建并运行容器
-# 不适用于Docker部署
-# docker run -d --restart=always -p 80:81 -v ./themes:/blog/themes -v ./hugo.toml:/blog/hugo.toml -v ./content:/blog/content -v ./static:/blog/static --name blog blog
-# 功能性容器
-docker run -d -v ./themes:/blog/themes -v ./hugo.toml:/blog/hugo.toml -v ./content:/blog/content -v ./static:/blog/static -v ./public:/blog/public --name blog blog 
+    # 删除容器
+    docker stop blog && docker rm blog
+    # 删除镜像
+    docker rmi blog
+    # 构建镜像
+    docker build -t blog .
+    # 创建并运行容器
+    # 不适用于Docker部署
+    # docker run -d --restart=always -p 80:81 -v ./themes:/blog/themes -v ./hugo.toml:/blog/hugo.toml -v ./content:/blog/content -v ./static:/blog/static --name blog blog
+    # 功能性容器
+    docker run -d -v ./themes:/blog/themes -v ./hugo.toml:/blog/hugo.toml -v ./content:/blog/content -v ./static:/blog/static -v ./public:/blog/public --name blog blog 
 
-# Nginx 如果配置好了，可直接访问网站查看部署更新
+    # Nginx 如果配置好了，可直接访问网站查看部署更新
+fi
