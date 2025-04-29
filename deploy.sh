772a40de7226c802d030360f6ec54870e5b8b6a2
templechan <templechan@126.com>
2025-04-29 15:16:05 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -64,8 +64,9 @@ if [ -d /usr/local/src/blog ]; then
         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
         # 加载 nvm（确保当前 shell 环境能使用 nvm）
-        export NVM_DIR="$HOME/.nvm"
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" >> ~/.bashrc
+        echo 'export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> ~/.bashrc
         source ~/.bashrc
 
         nvm install 20.19.1
